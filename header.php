3e4f39e9e3e3447a70be7904e4303f90414db430
yangyang01011001 <yangyang01011001@gmail.com>
2016-03-02 23:39:21 -0600

    Update header.php

--- header.php
+++ header.php
@@ -7,16 +7,14 @@
 
 <div class="header">
 <ul>
-  <li><a class="active" href="http://www.futuregenerationmedical.com">Home</a></li>
-  <li><a href="http://futuregenerationmedical.com/about_us.html">ABOUT US</a></li>
-  <li><a href="http://futuregenerationmedical.com/who_we_are.html">WHO WE ARE</a></li>
-  <li><a href="http://futuregenerationmedical.com/services.html">SERVICES</a></li>
-  <li><a href="http://futuregenerationmedical.com/cgi-bin/future_gm_product_nologin.php">INVENTORY</a></li>
-  <li><a href="http://futuregenerationmedical.com/contact_us.html">CONTACT US</a></li>
+  <li><a class="active" href="http://localhost:8888">Home</a></li>
+  <li><a href="http://localhost:8888/about_us.html">ABOUT US</a></li>
+  <li><a href="http://localhost:8888/media.html">Media</a></li>
+  <li><a href="http://localhost:8888/contact_us.html">CONTACT US</a></li>
   <li>
   <?php
   if (isset($_SESSION['username'])) { ?>
-  	<form name="HeaderLoginForm" method="POST" action="future_generation_medical.php">
+  	<form name="HeaderLoginForm" method="POST" action="Main_Controller.php">
 	<a>
 	Dear <?php echo $_SESSION['username']; ?></a>
 	<input type="hidden" name="action" value="header_login"/>
@@ -25,17 +23,11 @@
   } 
   else {
   ?>
-  	<a href="http://futuregenerationmedical.com/cgi-bin/future_generation_medical.php">SIGN UP</a>
+  	<a href="localhost:8888">SIGN UP</a>
   <?php
   }
   ?>
   </li>
-  <!--
-  <ul style="float:right;list-style-type:none;">
-    <li><a href="#about">About</a></li>
-    <li><a href="#login">Login</a></li>
-  </ul>
-  -->
 </ul>
 </div>
 
